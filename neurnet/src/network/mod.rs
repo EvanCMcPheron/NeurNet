29ca643944f59fbb107f6d587a7ef7f93bb87c70
Evan McPheron <107580262+EvanCMcPheron@users.noreply.github.com>
2022-10-13 16:16:11 +0000

    Added Randomization

--- neurnet/src/network/mod.rs
+++ neurnet/src/network/mod.rs
@@ -9,11 +9,20 @@ struct Layer {
 }
 
 impl<F: Fn(f64) -> f64> Network<F> {
-    pub fn new(shape: Vec<usize>, activation_fn: F) -> Network<F> {
+    pub fn new(
+        shape: Vec<usize>,
+        activation_fn: F,
+        weights_range: (f64, f64),
+        biases_range: (f64, f64),
+    ) -> Network<F> {
         let mut layers: Vec<Layer> = vec![];
         for i in 1..shape.len() {
             //1..shape.len() bcs the input layer shouldn't be an actual layer
-            layers.push(Layer::new(*&shape[i], *&shape[i-1]))
+            layers.push({
+                let mut layer = Layer::new(*&shape[i], *&shape[i - 1]);
+                layer.randomize(weights_range, biases_range);
+                layer
+            })
         }
         Network {
             activation_fn,
@@ -22,56 +31,113 @@ impl<F: Fn(f64) -> f64> Network<F> {
         }
     }
     pub fn pulse(&self, input: Vec<f64>) -> Vec<f64> {
-        if input.len() < self.shape[0] {panic!("Network was passed more inputs than there are neurons in the first layer of the network");}
+        if input.len() < self.shape[0] {
+            panic!("Network was passed more inputs than there are neurons in the first layer of the network");
+        }
         let mut layer_output = input;
         for layer in self.layers.iter() {
             layer_output = layer.pulse(layer_output, self);
         }
         layer_output
     }
+    pub fn set_weight(
+        &mut self,
+        layer: usize,
+        neuron: usize,
+        prev_layer_neuron: usize,
+        weight: f64,
+    ) {
+        //! Sets the weight of a neuron connection between a neuron in *layer* and a neuron in the previous layer. Note that the input layer isn't counted as a layer, so layer=0 would actually be accessing the second layer.
+        //! # Panics
+        //! <ul>
+        //! <li> Attempting to mutate the weight to or from a non-existant neuron.
+        //! </ul>
+        self.layers
+            .get_mut(layer)
+            .unwrap()
+            .set_weight(neuron, prev_layer_neuron, weight);
+    }
+    pub fn get_weight(
+        &self,
+        layer: usize,
+        neuron: usize,
+        prev_layer_neuron: usize,
+    ) -> Option<&f64> {
+        self.layers
+            .get(layer)?
+            .get_weight(neuron, prev_layer_neuron)
+    }
+    pub fn set_bias(&mut self, layer: usize, neuron: usize, bias: f64) {
+        self.layers.get_mut(layer).unwrap().set_bias(neuron, bias);
+    }
+    pub fn get_bias(&self, layer: usize, neuron: usize) -> Option<&f64> {
+        self.layers.get(layer)?.get_bias(neuron)
+    }
+    pub fn randomize(&mut self, weights_range: (f64, f64), biases_range: (f64, f64)) {
+        for layer in self.layers.iter_mut() {
+            layer.randomize(weights_range, biases_range);
+        }
+    }
 }
 
 impl Layer {
-    pub fn new(layer_size: usize, prev_layer_size: usize) -> Layer {
+    fn new(layer_size: usize, prev_layer_size: usize) -> Layer {
         Layer {
-            weights: vec![vec![1.0; prev_layer_size];layer_size],
-            biases: vec![0.0; layer_size]
+            weights: vec![vec![1.0; prev_layer_size]; layer_size],
+            biases: vec![0.0; layer_size],
         }
     }
-    pub fn pulse<F: Fn(f64) -> f64>(&self, input: Vec<f64>, network: &Network<F>) -> Vec<f64> {
+    fn pulse<F: Fn(f64) -> f64>(&self, input: Vec<f64>, network: &Network<F>) -> Vec<f64> {
         let mut output_buf: Vec<f64> = vec![];
         for neuron_indx in 0..self.biases.len() {
             //For every neuron in layer
-            output_buf.push(
-                (network.activation_fn)({
+            output_buf.push((network.activation_fn)(
+                {
                     //Sum the weighted inputs
                     let mut sum: f64 = 0.0;
                     for input_indx in 0..input.len() {
                         //for every input
-                        sum += input[input_indx] * *(self.get_weight(neuron_indx, input_indx).unwrap());
+                        sum += input[input_indx]
+                            * *(self.get_weight(neuron_indx, input_indx).unwrap());
                     }
                     sum
-                } + self.get_bias(neuron_indx).unwrap())
-            );
+                } + self.get_bias(neuron_indx).unwrap(),
+            ));
         }
         output_buf
     }
-    pub fn set_weight(&mut self, neuron: usize, prev_layer_neuron: usize, weight: f64) {
-        *(self.weights
+    fn set_weight(&mut self, neuron: usize, prev_layer_neuron: usize, weight: f64) {
+        *(self
+            .weights
             .get_mut(neuron)
             .expect("A viable neuron ID")
             .get_mut(prev_layer_neuron)
             .expect("A viable ID to a neuron in the previous layer")) = weight;
     }
-    pub fn get_weight(&self, neuron: usize, prev_layer_neuron: usize) -> Option<&f64> {
-        Some(self.weights
-            .get(neuron)?
-            .get(prev_layer_neuron)?)
+    fn get_weight(&self, neuron: usize, prev_layer_neuron: usize) -> Option<&f64> {
+        Some(self.weights.get(neuron)?.get(prev_layer_neuron)?)
     }
-    pub fn set_bias(&mut self, neuron: usize, bias: f64) {
+    fn set_bias(&mut self, neuron: usize, bias: f64) {
         *(self.biases.get_mut(neuron).expect("A valid neuron ID")) = bias;
     }
-    pub fn get_bias(&self, neuron: usize) -> Option<&f64> {
+    fn get_bias(&self, neuron: usize) -> Option<&f64> {
         Some(self.biases.get(neuron)?)
+    }
+    fn randomize(&mut self, weights_range: (f64, f64), biases_range: (f64, f64)) {
+        fn rand_float(range: (f64, f64)) -> f64 {
+            use rand::random;
+            let mut buf: f64 = random();
+            buf *= range.1 - range.0;
+            buf += range.0;
+            buf
+        }
+        for neuron in self.weights.iter_mut() {
+            for weight in neuron.iter_mut() {
+                *weight = rand_float(weights_range);
+            }
+        }
+        for bias in self.biases.iter_mut() {
+            *bias = rand_float(biases_range);
+        }
     }
 }
--- neurnet_example/src/main.rs
+++ neurnet_example/src/main.rs
@@ -2,9 +2,36 @@ use neurnet::*;
 
 fn main() {
     let mut nn = network::Network::new(
-        vec![2,2,1], 
-        |x: f64| -> f64 {1.0 / (1.0 + (-x).exp())}
+        vec![2, 2, 1],
+        |x: f64| -> f64 { 1.0 / (1.0 + (-x).exp()) },
+        (-2.0, 2.0),
+        (-5.0, 5.0),
     );
     let input = vec![1.0, 1.0];
-    println!("Network Input: {:?}\nNetwork Output: {:?}", &input, nn.pulse(input.clone()));
+    println!(
+        "Network Input: {:?}\nNetwork Output: {:?}",
+        &input,
+        nn.pulse(input.clone())
+    );
+    let ranges: ((f64, f64), (f64, f64)) = ((-2.0, 2.0), (-5.0, 5.0));
+    for _ in 0..10000 {
+        nn.randomize(ranges.0, ranges.1);
+        println!(
+            "\nNetwork Input: {:?}\nNetwork Output: {:?}",
+            &input,
+            nn.pulse(input.clone())
+        );
+        let check: (f64, f64) = (
+            *nn.get_weight(0, 0, 0).unwrap(),
+            *nn.get_bias(0, 0).unwrap(),
+        );
+        println!("Some weight: {}\nSome Bias: {}", check.0, check.1,);
+        if check.0 < ranges.0 .0
+            || check.0 > ranges.0 .1
+            || check.1 < ranges.1 .0
+            || check.1 > ranges.1 .1
+        {
+            panic!("something is wrong!");
+        }
+    }
 }
