454760a944c958643e14fcadd28e18e3e7cd738a
Evan McPheron <107580262+EvanCMcPheron@users.noreply.github.com>
2022-10-17 19:25:28 +0000

    Changed network to allow training multi-output networks.

--- neurnet_example/src/main.rs
+++ neurnet_example/src/main.rs
@@ -7,7 +7,7 @@ fn main() {
     std::io::stdin().read_line(&mut buf_rate).unwrap();
     let rate: f64 = buf_rate.trim().parse().unwrap();
     let mut nn = Network::new(
-        vec![1, 5, 4, 5, 4, 3, 2],
+        vec![1, 5, 4, 5, 4, 6, 2],
         |x| if x > 0.0 { x } else { 0.01 * x },
         Some(|x| if x > 0.0 { 1.0 } else { 0.01 }),
         (-2.0, 2.0),
@@ -34,12 +34,12 @@ fn main() {
     );
     println!("Training...");
     println!("{:?}", nn.test(&ds));
-    for i in 0..5000 {
+    for i in 0..500 {
         nn.train(&ds, rate, 2);
         println!("{}: {:?}", i, nn.test(&ds));
     }
-    for i in 0..=100 {
-        println!("({}, {})", i, nn.pulse(vec![i as f64])[0]);
+    for i in -100..=100 {
+        println!("({}, {:?})", i, nn.pulse(vec![i as f64]));
     }
     let mut buf = String::new();
     println!("Press enter to exit...");
